62e54f4ff85f141fbf2dd654bc53414818d277b1
matmat92 <promathieuthiry@gmail.com>
2019-11-23 11:44:51 +0100

    Updates

---
+++ precache-manifest.57a2de82c220789367bd8dd157077007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce013c519617a29f36fb81eff45e5bdf",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "8dbaf37b40856a987564",
+    "url": "/news/static/css/2.304f0c35.chunk.css"
+  },
+  {
+    "revision": "e22bb32f87d35d5bcfbc",
+    "url": "/news/static/css/main.c9525f28.chunk.css"
+  },
+  {
+    "revision": "8dbaf37b40856a987564",
+    "url": "/news/static/js/2.6c9c7aee.chunk.js"
+  },
+  {
+    "revision": "e22bb32f87d35d5bcfbc",
+    "url": "/news/static/js/main.b6231aa0.chunk.js"
+  },
+  {
+    "revision": "a25e839665a971dcd35e",
+    "url": "/news/static/js/runtime-main.b56a2a80.js"
+  }
+]);
